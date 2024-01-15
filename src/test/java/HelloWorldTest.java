c930653261e73cb8e649e59b2641daecd70d5f34
Malex <malexx2996@gmail.com>
2024-01-15 14:29:42 +0300

    Ex4: GET-запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public void testHelloFrom(){
-    System.out.println("Hello from Alex");
+    public void testGetText(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
